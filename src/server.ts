75e4fa678139c3da3b6b27042aad32953416e718
survikrowa <szymonhrabia60@gmail.com>
2022-08-16 22:10:10 +0200

    feat/add-autoload-plugins

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import fastify from 'fastify';
+import fastifyAutoload from '@fastify/autoload';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 export const app = fastify({
-  logger: true,
+	logger: true,
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.register(fastifyAutoload, { dir: join(__dirname, 'plugins') });
+
 app.get('/', (_, reply) => {
-  reply.send('hello frontlive');
+	reply.send('hello frontlive');
 });
