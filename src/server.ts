0e9fb948b5fd4fa2b3a68745c70fba30e9637d6b
survikrowa <szymonhrabia60@gmail.com>
2022-08-20 23:50:31 +0200

    fix/esm-modules

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import fastify from 'fastify';
 import fastifyAutoload from '@fastify/autoload';
+import fastifySwagger from '@fastify/swagger';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const app = fastify({
 	logger: true,
 });
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.register(fastifyAutoload, { dir: join(__dirname, 'plugins') });
-
+app.register(fastifyAutoload, {
+	dir: join(__dirname, 'plugins'),
+	forceESM: true,
+});
+app.register(fastifySwagger, { routePrefix: '/documentation' });
 app.get('/', (_, reply) => {
 	reply.send('hello frontlive');
 });
